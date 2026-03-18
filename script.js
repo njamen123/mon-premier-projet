72b827b9cdb4245c080593d851ac4c6466d70029
@njamen 123 <prestigenjamen@gmail.com>
2026-03-18 15:42:01 +0100

    Ajout du site vitrine pour l'Hôtel Azur

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Bienvenue à l'Hôtel Azur !");
+
+// Affiche un message de bienvenue dans la console
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Bienvenue sur le site de l\'Hôtel Azur !');
+});
